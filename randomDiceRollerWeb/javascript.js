65cf5ec2f8b91f67b50bf87f1feafa8b7101ded3
DanielSpiering <97045655+DanielSpiering@users.noreply.github.com>
2023-09-14 16:24:59 -0500

    shows previous result after a new roll

--- randomDiceRollerWeb/javascript.js
+++ randomDiceRollerWeb/javascript.js
@@ -5,15 +5,31 @@ var randomNumber;
 submitButton.addEventListener('click', rollDiceButton);
 
 function rollDiceButton(e) {
+    //check the previous roll and show the result
+    let previousRoll = document.getElementById('dice-sum').innerText;
+    document.getElementById('previous-dice-roll').innerText = "";
+
+    if (previousRoll == "") {
+        document.getElementById('previous-dice-roll').innerText = "Previous Dice Sum: 0";
+    } else {
+        document.getElementById('previous-dice-roll').innerText += `Previous ${previousRoll}`;
+    }//end if
+    
     e.preventDefault();
     rollDice();
+   
+    
 }//end function
 function rollDice() {
+    //reset the dice output list
     document.getElementById('dice-output').innerHTML = "";
+
+    //get the user input values
     const diceAmountTextBox = document.getElementById('dice-amount').value;
     const diceSizeTextBox = document.getElementById('dice-sides').value;
     let sum = 0;
 
+    //create a random number for each dice rolled and add them together
     for (var index = 0; index < diceAmountTextBox; index++) {
         randomNumber = Math.floor(Math.random() * (diceSizeTextBox - 1 + 1) + 1);
         sum += randomNumber;
@@ -22,6 +38,10 @@ function rollDice() {
         document.querySelector('ul').appendChild(node);
         
     }//end for
+
+    //erase previous sum and print new one
     document.getElementById('dice-sum').innerText = "";
     document.getElementById('dice-sum').innerText += `Dice Sum: ${sum}`;
+    return sum;
 }//end function
+
